5ad214e7f4c856a81948a515cb7c1aa2910bcb13
eloyrj <eloy.rodriguezj@edu.uah.es>
2023-11-02 12:11:42 +0100

    bag y demas

--- Scripts/conectar.m
+++ Scripts/conectar.m
@@ -1,3 +1,3 @@
-rosinit('http://172.29.30.179:11311', 'NodeHost', '172.29.29.51')
-%               IP UBUNTU (ifconfig)               IP WINDOWS (ipconfig)
+rosinit('172.29.30.179', 'NodeHost', '172.29.29.111')
+%          172.29.29.113 IP UBUNTU (ifconfig)       172.29.29.111 IP WINDOWS (ipconfig)
 %               O ROBOT (pegatina)
