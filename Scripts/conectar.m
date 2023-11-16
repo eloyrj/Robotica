f534652d43a1bf42391ceb861531b62ffda3301e
eloyrj <48245099+eloyrj@users.noreply.github.com>
2023-11-16 11:33:14 +0100

    entrega

--- Scripts/conectar.m
+++ Scripts/conectar.m
@@ -1,3 +1,3 @@
-rosinit('172.22.32.125', 'NodeHost', '172.22.31.104')
+rosinit('172.22.76.26', 'NodeHost', '172.22.31.104')
 %          172.29.29.113 IP UBUNTU (ifconfig)       172.29.29.111 IP WINDOWS (ipconfig)
 %               O ROBOT (pegatina)
